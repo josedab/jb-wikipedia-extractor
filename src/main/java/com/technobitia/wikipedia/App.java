9b930b7086fc146cc0c3e9daa62d8dee04a39b40
Jose David Baena <josedab@gmail.com>
2015-02-07 23:09:07 +0000

    IS-1 Maven basic project setup

---
+++ src/main/java/com/technobitia/wikipedia/App.java
@@ -0,0 +1,13 @@
+package com.technobitia.wikipedia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
