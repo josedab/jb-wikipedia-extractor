0aab204e8cecb41f74e9cb63e5a87c72d4e80d53
Jose David Baena <josedab@gmail.com>
2015-02-08 18:30:31 +0000

    NT Added content type to the wikipedia request configuration

--- src/main/java/com/technobitia/wikipedia/request/WikipediaRequest.java
+++ src/main/java/com/technobitia/wikipedia/request/WikipediaRequest.java
@@ -1,12 +1,18 @@
 package com.technobitia.wikipedia.request;
 
 public class WikipediaRequest {
+    
+    public static final String SIDEBAR_REQUEST_TYPE = "sidebar";
+    public static final String CONTENT_TYPE = "html";
+    
     private final String url;
     private final String requestedType;
+    private final String contentType;
 
     private WikipediaRequest(Builder requestBuilder) {
         this.url = requestBuilder.url;
         this.requestedType = requestBuilder.requestedType;
+        this.contentType = requestBuilder.contentType;
     }
 
     public String getUrl() {
@@ -17,16 +23,26 @@ public class WikipediaRequest {
         return requestedType;
     }
 
+    public String getContentType() {
+        return contentType;
+    }
+
     public static class Builder {
         private final String url;
-        private String requestedType = "sidebar";
+        private String requestedType = SIDEBAR_REQUEST_TYPE;
+        private String contentType = CONTENT_TYPE;
 
-        private Builder(String url) {
+        public Builder(String url) {
             this.url = url;
         }
 
-        private Builder withRequestedType(String type) {
+        public Builder withRequestedType(String type) {
             this.requestedType = type;
+            return this;
+        }
+        
+        public Builder withContentType(String contentType) {
+            this.contentType = contentType;
             return this;
         }
 
